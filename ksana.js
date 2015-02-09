ede6cedc451ddb4a3c2a5e03e432e4cdc13eb870
yujunyak <yujunyak@gmail.com>
2015-02-09 15:47:49 +0800

    first build

---
+++ ksana.js
@@ -0,0 +1,42 @@
+jsonp_handler({
+ "version": "1",
+ "build": 3,
+ "title": "myapp",
+ "minruntime": 1,
+ "baseurl": "http://yakeugene@rawgit.com/yakeugene/myapp/master/",
+ "description": "",
+ "files": [
+  "index.html",
+  "index.css",
+  "bundle.js",
+  "jquery.js",
+  "react-with-addons.js",
+  "ksana.js",
+  "bootstrap.min.js",
+  "bootstrap.min.css",
+  "sample.kdb"
+ ],
+ "filesizes": [
+  552,
+  64,
+  155641,
+  277980,
+  645243,
+  771,
+  35607,
+  113502,
+  97570
+ ],
+ "filedates": [
+  "2015-02-09T07:31:07.179Z",
+  "2015-02-09T07:31:07.173Z",
+  "2015-02-09T07:39:23.422Z",
+  "2015-02-09T07:31:07.269Z",
+  "2015-02-09T07:31:07.422Z",
+  "2015-02-09T07:32:08.750Z",
+  "2015-02-09T07:31:07.148Z",
+  "2015-02-09T07:31:07.104Z",
+  "2015-02-09T07:31:49.171Z"
+ ],
+ "date": "2015-02-09T07:39:24.382Z"
+})
